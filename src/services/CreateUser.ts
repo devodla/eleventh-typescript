6bcfe2a71156ef177af6bb746418a812957db503
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-04-11 00:55:32 -0300

    feat(main) add types of interfaces

---
+++ src/services/CreateUser.ts
@@ -0,0 +1,26 @@
+/**
+ * Para criar: name, email, password
+ */
+
+ interface TechObject {
+   title: string;
+   experience: number;
+ }
+
+ interface CreateUserData {
+   name?: string;
+   email: string;
+   password: string;
+   techs: Array<string | TechObject>;
+   hobbies?: string[];
+ }
+
+ export default function createUSer({ name, email, password }: CreateUserData) {
+   const user = {
+     name,
+     email,
+     password,
+   }
+
+   return user;
+ }
